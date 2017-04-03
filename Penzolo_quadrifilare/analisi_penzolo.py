45551a5c7088ae3e85a3a2944c7a8a4c325ceed7
Francesco Sacco <francesco215@live.it>
2017-04-03 12:08:36 +0200

    aggiunti dati volano e modificato file di analisi pendolo quadrifilare

--- Penzolo_quadrifilare/analisi_penzolo.py
+++ Penzolo_quadrifilare/analisi_penzolo.py
@@ -1,10 +1,12 @@
+
 import numpy as np
 from scipy import loadtxt
-import matplotlib.pyplot as plt
+import pylab 
 from scipy.optimize import curve_fit 
 larghezza_bandiera=np.array([0.0210,0.0005])
-distanza_bandiera=np.array([0.1164,0.005])
-g=9.81
+distanza_bandiera=np.array([1.164,0.01])
+distanzaCm=np.array([1.117,0.01])
+g=9.806
 #plt.ion() # interactive on
 misure_angoli=['dati/misura_angolo1.txt','dati/misura_angolo2.txt','dati/misura_angolo3.txt','dati/misura_angolo4.txt','dati/misura_angolo5.txt']
 piccoleOscillazioni='dati/piccole_oscillazioni1.txt'
@@ -19,7 +21,20 @@ def lettura(file):
 		tPassaggio=np.insert(tPassaggio,len(tPassaggio),tempo[i-1]-tempo[i-2])
 	return Periodo,tPassaggio
 
-def funzione():
-	v0=
-	theta=np.arcos(1-v0**2/(2*g*distanza_bandiera))
 
+def fPeriodo(tempoTransito,distanza_cm,p1,p2):
+	v0=larghezza_bandiera[0]*distanza_cm/(tempoTransito*distanza_bandiera[0])
+	theta=np.arccos(1-v0**2/(2*g*distanza_cm))
+	return 2*np.pi*np.sqrt(distanza_cm/g)*(1+theta**2.*p1+p2*theta**4.)
+
+#print(len(lettura(misure_angoli[0])[1]),len(lettura(misure_angoli[0])[0]))
+p0=np.array([1.11,1/16,11/3072])
+popt,pcov=curve_fit(fPeriodo,lettura(misure_angoli[0])[1],lettura(misure_angoli[0])[0],p0)
+dl=pcov.diagonal()
+print(lettura(misure_angoli[0]),[0])
+
+v0=larghezza_bandiera[0]*popt[0]/(lettura(misure_angoli[0])[1]*distanza_bandiera[0])
+theta=np.arccos(1-v0**2/(2*g*popt[0]))
+
+pylab.plot(theta,lettura(misure_angoli[0])[0],'.')
+pylab.show()
--- Yoyo/analisi.py
+++ Yoyo/analisi.py
@@ -1,4 +1,17 @@
 import numpy as np
 import matplotlib
+import pylab
+from scipy.optimize import curve_fit
+dati=['dati/ruota_libera.txt']
+accelerazione=([])
 massa_piattello=np.array([0.023011,0.000001])
+raggio_pesetto=np.array([0.02006,0.000005])
+raggio_cerchio=np.array([0.16],0.0000075)
+tempi,velocità=np.loadtxt(dati[0],unpack='true')
 
+for i in range (0,len(tempi)-2):
+	accelerazione=np.insert(accelerazione,len(accelerazione),(velocità[i+1]-velocità[i])/(tempi[i+1]-tempi[i]))
+
+pylab.plot(tempi,velocità)
+pylab.plot(tempi[:-2],accelerazione)
+pylab.show()
