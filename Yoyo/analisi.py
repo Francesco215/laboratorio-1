d1ed089870522724ef546ce862795afdc6dfc883
Francesco Sacco <francesco215@live.it>
2017-06-23 12:20:44 +0200

    finalmente il fit funziona, ma non ho sampato i dati

--- Yoyo/analisi.py
+++ Yoyo/analisi.py
@@ -1,24 +1,41 @@
+"""   ISTRUZIONI ALL'USO
+#PER IL VOLANO LIBERO
+1)Mettere in dati[0] il file del volano libero
+#PER QUELLO OSCILLATORIO
+1)prendere il file di acquisizione
+2)tagliare l'ultima parte se è troppo brutta e metterlo
+3)nel vettore dei dati dei dati.
+4)mettere nel vettore delle masse il suo peso in kg nella medesima posizione
+5)indicare la posizione in nummero
+6)pregare
+7)eseguire il programma
+"""
+
 import numpy as np
 import matplotlib
 import pylab
 from scipy.optimize import curve_fit
-dati=['dati/ruota_libera.txt','dati/150.txt','dati/300.txt']
+dati=['dati/ruota_libera.txt','dati/150.txt','dati/300.txt','dati/400.txt']
 massa_piattello=np.array([0.023011,0.000001])
-masse=([0,0.15,0.3])
+masse=([0,0.15,0.3,0.5])
 raggio_pesetto=np.array([0.02006,0.000005])
 raggio_cerchio=np.array([0.16,0.0000075])
 g=9.81
-
+nummero=2
+taglio=2
 
 #lettura[m][n][l] indica m=il file, se n=0 il tempo, ne n=1 omega, l indica l'essesimo elemento della lista
-lettura=[np.loadtxt(dati[0],unpack='true'),np.loadtxt(dati[1],unpack='true'),np.loadtxt(dati[2],unpack='true')]
+lettura=[]
+for i in range (0,len(dati)):
+	estrazione=np.loadtxt(dati[i],unpack='true')
+	lettura.append(estrazione)
 
 #faccio tutto quello che c'è da fare nel caso in cui la ruota è libera
 def fRuotaLibera(tempo,omega0,tau,mInerzia):
 	return omega0-(tempo*tau)/mInerzia
-"""
-datiRuotaLibera,varRuotaLibera=curve_fit(fRuotaLibera,lettura[0][0],lettura[0][1])
 
+datiRuotaLibera,varRuotaLibera=curve_fit(fRuotaLibera,lettura[0][0],lettura[0][1])
+"""
 pylab.plot(lettura[0][0],lettura[0][1])
 print(datiRuotaLibera)
 x=np.linspace(0,100,200)
@@ -26,14 +43,13 @@ y=fRuotaLibera(x,*datiRuotaLibera)
 pylab.plot(x,y)
 pylab.title("Ruota libera")
 """
-
 #adesso si fa le oscillazioni normali
 #creo l'equazione che descrive il moto dell'oggetto quande sale e quando scende
 def YoyoSu(tempo,omega0,tau,mInerzia):
-	return tempo*((massa_piattello[0]+masse[1])*g*raggio_pesetto[0]-tau)/(mInerzia+masse[1]*raggio_pesetto[0]**2)+omega0
+	return tempo*((massa_piattello[0]+masse[nummero])*g*raggio_pesetto[0]-tau)/(mInerzia+masse[nummero]*raggio_pesetto[0]**2)+omega0
 
 def YoyoGiu(tempo,omega0,tau,mInerzia):
-	return -tempo*((massa_piattello[0]+masse[1])*g*raggio_pesetto[0]+tau)/(mInerzia+masse[1]*raggio_pesetto[0]**2)+omega0
+	return -tempo*((massa_piattello[0]+masse[nummero])*g*raggio_pesetto[0]+tau)/(mInerzia+masse[nummero]*raggio_pesetto[0]**2)+omega0
 
 
 #creo una funzione che trova i massimi e i minimi
@@ -68,10 +84,10 @@ pylab.ylabel("accelerazione angolare [rad/s^2]")
 #e ora faccio un mucchio di fit
 def fit(lista):
 	massimi,minimi=RicercaMaxMin(lista)
-	pylab.plot(lista[0],lista[1])
+	pylab.plot(lista[0],lista[1],'-',color='grey',lw=2)
 	a=0
 	b=0
-	while a<len(massimi) and b<len(minimi):
+	while a<len(massimi)-taglio and b<len(minimi)-taglio:
 		if massimi[a]<minimi[b]:
 			fit,varFit=curve_fit(YoyoGiu,lista[0][massimi[a]:minimi[b]+1],lista[1][massimi[a]:minimi[b]+1])
 			x=np.linspace(lista[0][massimi[a]],lista[0][minimi[b]],100)
@@ -82,9 +98,11 @@ def fit(lista):
 			x=np.linspace(lista[0][minimi[b]],lista[0][massimi[a]],100)
 			y=YoyoSu(x,*fit)
 			b=b+1
-		pylab.plot(x,y)
+		pylab.plot(x,y,'--',color='black',lw=3)
+		if a+b==1:
+			print("parametri di fit",fit,"diagonale covarianza",varFit.diagonal())
 
-fit(lettura[1])
+fit(lettura[nummero])
 
 
 
