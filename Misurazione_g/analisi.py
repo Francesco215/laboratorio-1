c7f4a53354c4100a17f32729511e23d598282fbd
Francesco Sacco <francesco215@live.it>
2017-06-28 10:22:20 +0200

    finita analisi dati

--- Misurazione_g/analisi.py
+++ Misurazione_g/analisi.py
@@ -1,8 +1,7 @@
 import numpy as np
-import matplotlib
 import pylab
-from scipy.odr import odrpack
 from scipy.optimize import curve_fit
+from scipy import stats
 #masse stimate in grammi
 g=9.807
 massaPiattello=7.87
@@ -23,6 +22,7 @@ tempi=([[6.85,6.88,6.83,6.83,8.80,6.72,6.87,6.77,6.73],
 		[8.74,8.70,8.75,8.75,8.63,8.74,8.72,8.63,8.73],
 		[9.53,9.50,9.53,9.59,9.57,9.51,9.50,9.57,9.49]])
 #divido i tempi in 10 per trovare 10 periodi
+
 periodi=([])
 devPeriodi=([])
 for i in range (0,len(tempi)):
@@ -36,7 +36,11 @@ def Periodo(m,k,k0):
 	return T2
 
 k,sk=curve_fit(Periodo,masse,periodi**2,sigma=devPeriodi)
+achi2=((periodi**2-Periodo(masse,*k))/devPeriodi).sum()
+
 print("\n\nk=",k[0],"+-",sk[0][0])
+print('chi2=',achi2,'pValue=',stats.chi2.cdf(achi2,len(periodi)))
+
 
 pylab.errorbar(masse,periodi**2,yerr=devPeriodi,fmt='.')
 x=np.linspace(masse[0]-0.003,masse[len(masse)-1]+0.003,100)
@@ -50,8 +54,11 @@ pylab.show()
 def fitG(m,a,p0):
 	return p0 + m*a
 popt,mammt=curve_fit(fitG,masse,allungamenti)
+bchi2=(((allungamenti-fitG(masse,*popt))/0.005)**2).sum()
 
-print('\n\nk/g=',popt[0],"+-",mammt[0][0],"\n\n")
+print('\n\ng/k=',popt[0],"+-",mammt[0][0])
+print("chi2=",bchi2,'pValue=',stats.chi2.cdf(bchi2,len(allungamenti)))
+
 x=np.linspace(masse[0]-0.003,masse[len(masse)-1]+0.003,100)
 y=fitG(x,*popt)
 pylab.plot(x,y)
@@ -60,3 +67,8 @@ pylab.title("Grafico che mostra l'allungamento in funzione delle masse")
 pylab.xlabel("m[kg]")
 pylab.ylabel("∆l[m]")
 pylab.show()
+
+g=popt[0]*k[0]
+dg=popt[0]*sk[0][0]+k[0]*mammt[0][0]
+
+print ('\n\ng=',g,'+-',dg,'\n\n')
